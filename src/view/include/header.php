3917e414c02a6b190b5e634efb83006e54fc5b26
Tariq <selawi.be@gmail.com>
2021-04-19 11:07:55 +0200

    deploy to heroku

--- src/view/include/header.php
+++ src/view/include/header.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300&display=swap" rel="stylesheet">
     <!-- link css -->
-    <link rel="stylesheet" href="/src/assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
