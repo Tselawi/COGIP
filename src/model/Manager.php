cece6cf2a7d34b466b12460fd97cd3c583fcdeac
Tariq <selawi.be@gmail.com>
2021-08-19 09:41:26 +0200

    fix the database

--- src/model/Manager.php
+++ src/model/Manager.php
@@ -15,7 +15,7 @@ class Manager
 
 
         try{
-            $db = new PDO("mysql:host=remotemysql.com;dbname=YlD1WTD8m9;port=3306","YlD1WTD8m9","nqgb2PnX1W");
+            $db = new PDO("mysql:host=remotemysql.com;dbname=kujV563UgE;port=3306","kujV563UgE","hWKl79lhCF");
             // $db = new PDO("mysql:host=mysql;dbname=COGIP;port=3306","root","root");
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
